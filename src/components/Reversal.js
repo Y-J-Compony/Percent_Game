e76463e7f4ae00357ddb9b868fbbce4d7ef0c8ad
YJH2848 <yunj05763@gmail.com>
2022-12-26 09:14:55 +0900

    확률 조정

--- src/components/Reversal.js
+++ src/components/Reversal.js
@@ -2,18 +2,17 @@ import { useState } from "react";
 import Bank from "./BankBook";
 import * as S from "../style/style";
 import Store from "../page/Store";
-import { number } from "prop-types";
 
 export default function Btn() {
   const [bankMoney, setBankMoney] = useState(1000000);
-  const [moneys, setMoneys] = useState(100000);
+  const [moneys, setMoneys] = useState(0);
   const [page, setPage] = useState(true);
   const [store, setStore] = useState(true);
   const [outMoney, setOutMoney] = useState(0);
   const [chance, setChance] = useState(3);
   const [onOff, setOnOff] = useState(false);
   const [inMoney, setInMoney] = useState(0);
-  const [identity, setIdentity] = useState("");
+  const [persen, setPersen] = useState(1);
 
   const goStore = () => {
     setStore(!store);
@@ -21,26 +20,29 @@ export default function Btn() {
   const money = moneys.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   const Btn1 = () => {
     const rand = Boolean(Math.round(Math.random()));
+
     console.log(moneys);
     if (money == 0) {
       alert("통장에서 출금하십시요");
       window.location.replace("/sectionTwo#sectionTwo");
-    }
-    // else if (rand == false) {
-    //   const randomMoney = Math.floor(Math.random() * 9000 + 1);
-    //   setMoneys(moneys - randomMoney);
-
-    //   setChance(chance - 1);
-    // } else if (rand == true) {
-    //   const randomMoney = Math.floor(Math.random() * 8000 + 1);
-    //   setMoneys(moneys + randomMoney);
-    //   setChance(chance - 1);
-    // }
-    else if (rand == true) {
+    } else if (Math.random() < persen) {
       setMoneys(moneys * 2);
-    } else if (rand == false) {
-      setMoneys(0);
+
+      setPersen(persen - 0.25);
+      if (persen == 0.25) {
+        setPersen(persen - 0);
+      }
+      setChance(chance - 1);
+    } else {
+      setMoneys(moneys - moneys);
+      setChance(chance - 1);
     }
+
+    // else if (Math.random() < { persen }) {
+    //   setMoneys(moneys * 2);
+    // } else if (rand == false) {
+    //   setMoneys(0);
+    // }
     if (chance == 1) {
       console.log(chance);
       setOnOff(true);
@@ -72,7 +74,7 @@ export default function Btn() {
     <div>
       <S.Board>
         <S.Life>인생역전</S.Life>
-
+        <h1>확률 : {persen * 100}</h1>
         <S.MoneyBox>
           <S.Money>Your money : {money}₩</S.Money>
         </S.MoneyBox>
